d888cd9b7010bfb876ae21de99c454543e64b220
Aurel Feer <aurel.feer@gmail.com>
2018-08-13 20:34:45 +0200

    renamed routes

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,18 @@
 |
 */
 
-Route::get('/', 'AdminLoginController@index')->name('root');
-Route::post('/', 'AdminLoginController@login')->name('login');
-Route::get('/logout', 'AdminLoginController@logout')->name('logout');
+Route::get('/', 'LoginController@showAdminLogin')->name('adminLogin');
+Route::get('/login', 'LoginController@showLogin')->name('login');
+Route::post('/login', 'LoginController@login')->name('ajax_login');
+Route::post('/logout', 'LoginController@logout')->name('ajax_logout');
 
-Route::get('/games/', 'GameController@list')->name('listGames');
-Route::get('/games/create', 'GameController@store')->name('storeGame');
-Route::patch('/games/{gameId}/update', 'GameController@update')->name('updateGame');
-Route::get('/games/{gameId}/delete', 'GameController@delete')->name('deleteGame');
-Route::get('/games/{gameId}', 'GameController@index')->name('viewGame');
+//MARK: -- GAME RESOURCE
+Route::get('/games/', 'GameController@index')->name('listGames');
+Route::get('/games/{gameId}', 'GameController@show')->name('viewGame');
+
+//create a new game
+Route::post('/games', 'GameController@store')->name('ajax_storeGame');
+//update the game
+Route::patch('/games/{gameId}', 'GameController@update')->name('ajax_updateGame');
+//delete the game
+Route::delete('/games/{gameId}', 'GameController@destroy')->name('ajax_destroyGame');
