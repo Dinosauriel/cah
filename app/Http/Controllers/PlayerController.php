478e82d6040769286a470d21149abbfcfaf7938f
Aurel Feer <aurel.feer@gmail.com>
2018-08-13 15:59:13 +0200

    moved AdminLoginController to PlayerController

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Player;
+
 class PlayerController extends Controller
 {
     /**
@@ -13,7 +15,7 @@ class PlayerController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin_login');
     }
 
     /**
@@ -80,5 +82,45 @@ class PlayerController extends Controller
     public function destroy($id)
     {
         //
-    }
+	}
+	
+	/**
+	 * checks the password and logs in the user
+	 * 
+	 * @return: redirect to lobby
+	 */
+	public function login(Request $request) {
+
+		//validate the form
+		$this->validate($request, [
+			'password' => 'required'
+		]);
+
+		//verify the password
+		$adminPassword = config('auth.cah_admin_password');
+		$isCorrectPassword = Hash::check($request->admin_password, $adminPassword);
+		if (!$isCorrectPassword) {
+			return redirect()->route('root');
+		}
+
+		//create a new player
+		$player = Player::create([
+			'username' => 'hi',
+			'is_admin' => 1,
+		]);
+
+		//log In player
+		auth()->login($player);
+
+		return redirect()->route('listGames');
+	}
+
+	/**
+	 * logs out the current admin
+	 */
+	public function logout() {
+		auth()->logout();
+
+		return redirect()->route('root');
+	}
 }
